9b4585b172a756350d113b838ca94bd474c2827b
Richard Ernst Neumann <118728127+RichardENeumann@users.noreply.github.com>
2024-03-27 10:58:36 +0100

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Your Title Here",
+    }),
+  ],
+  output: {
+    filename: "./main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
