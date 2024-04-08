3147e6e0988f17a6d16e76788260dde0844e79cf
RichardENeumann <RichardNeumann@googlemail.com>
2024-04-08 21:04:10 +0200

    Refactor some more

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./gallery.css";
 
 import rLib from "./rLib.js";
-
 /*
 const btMenu1 = document.getElementById("btMenu1");
 const divMenu1 = document.getElementById("menu1");
@@ -10,16 +9,16 @@ btMenu1.addEventListener("click", () => {
   rLib.showDropdownMenu(divMenu1);
 });
 */
-
 rLib.htmlIncludeAll();
+
 rLib.galleryLoad();
 
-const btLeft = document.getElementById("btGalleryLeft");
-btLeft.addEventListener("click", () => {
+const btGalleryLeft = document.getElementById("btGalleryLeft");
+btGalleryLeft.addEventListener("click", () => {
   rLib.galleryShift(-1);
 });
 
-const btRight = document.getElementById("btGalleryRight");
-btRight.addEventListener("click", () => {
+const btGalleryRight = document.getElementById("btGalleryRight");
+btGalleryRight.addEventListener("click", () => {
   rLib.galleryShift(1);
 });
--- src/rLib-htmlInclude.js
+++ src/rLib-htmlInclude.js
@@ -11,16 +11,15 @@ async function htmlInclude(url) {
   return result;
 }
 
-// htmlIncludeAll() will parse the DOM for tags with the "rl-include" 
-// Attribute. Then it will fetch the resource at the attribute's given 
+// htmlIncludeAll() will parse the DOM for tags with the "rlInc"
+// Attribute. Then it will fetch the resource at the attribute's given
 // URL and replace the tag with the resulting text.
 export default function htmlIncludeAll() {
-  const tagsWithInclude = document.querySelectorAll("[rl-include]");
-  const includeRequests = Array.from(tagsWithInclude);
+  const tagsWithInclude = document.querySelectorAll("[rlInc]");
 
-  includeRequests.forEach(async (el) => {
+  [...tagsWithInclude].forEach(async (el) => {
     const target = el;
-    const url = el.attributes["rl-include"].value;
+    const url = el.attributes.rlInc.value;
     target.outerHTML = await htmlInclude(url);
   });
 }
